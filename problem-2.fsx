4e36ff05e397ee57f7a624a85c52c4130ef1ba5b
Humza Shahid <humzasaur@gmail.com>
2022-10-05 17:13:53 +0100

    fix problem 2 misunderstanding, but remember to optimise later

--- problem-2.fsx
+++ problem-2.fsx
@@ -1,11 +1,11 @@
-let rec fibonacci a b evenTotal =
+let rec fibonacci a b evenNums =
     let higher = if a > b then a else b
     let lower = if higher = a then b else a
     let sum = higher + lower
 
     match sum with
-    | sum when sum > 4000_000 -> evenTotal
-    | sum when (sum % 2 = 0) -> fibonacci sum higher sum
-    | sum -> fibonacci sum higher evenTotal
+    | sum when sum > 4000_000 -> evenNums
+    | sum when (sum % 2 = 0) -> fibonacci sum higher (evenNums @ [ sum ])
+    | sum -> fibonacci sum higher evenNums
 
-printfn "%i" (fibonacci 0 1 0)
+printfn "%i" (fibonacci 1 2 [ 1; 2 ] |> List.sum)
