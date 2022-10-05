21dcda730014fa08d94fa7498e827833524e0cc3
Humza Shahid <humzasaur@gmail.com>
2022-10-05 17:27:38 +0100

    optimise problem 2 by using accomulator evenTotal instead of outputting a list for (removes O(n) loop)

--- problem-2.fsx
+++ problem-2.fsx
@@ -1,11 +1,11 @@
-let rec fibonacci a b evenNums =
+let rec fibonacci a b evenTotal =
     let higher = if a > b then a else b
     let lower = if higher = a then b else a
     let sum = higher + lower
 
     match sum with
-    | sum when sum > 4000_000 -> evenNums
-    | sum when (sum % 2 = 0) -> fibonacci sum higher (evenNums @ [ sum ])
-    | sum -> fibonacci sum higher evenNums
+    | sum when sum > 4000_000 -> evenTotal
+    | sum when (sum % 2 = 0) -> fibonacci sum higher (evenTotal + sum)
+    | sum -> fibonacci sum higher evenTotal
 
-printfn "%i" (fibonacci 1 2 [ 1; 2 ] |> List.sum)
+printfn "%i" (fibonacci 1 2 2)
