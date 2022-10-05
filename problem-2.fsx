6654a4cf1f08fa523bd9f3351e3a2e4f367ad884
Humza Shahid <humzasaur@gmail.com>
2022-10-05 16:11:33 +0100

    fibonacci total adding only even terms

---
+++ problem-2.fsx
@@ -0,0 +1,11 @@
+let rec fibonacci a b evenTotal =
+    let higher = if a > b then a else b
+    let lower = if higher = a then b else a
+    let sum = higher + lower
+
+    match sum with
+    | sum when sum > 4000_000 -> evenTotal
+    | sum when (sum % 2 = 0) -> fibonacci sum higher sum
+    | sum -> fibonacci sum higher evenTotal
+
+printfn "%i" (fibonacci 0 1 0)
