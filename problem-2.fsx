fd9586038e2b704125b9afc9a2987b717f1d6d93
Humza Shahid <humzasaur@gmail.com>
2022-10-05 20:59:16 +0100

    optimise problem 2 even further

--- problem-2.fsx
+++ problem-2.fsx
@@ -1,3 +1,4 @@
+// initial brute force solution
 let rec fibonacci a b evenTotal =
     let higher = if a > b then a else b
     let lower = if higher = a then b else a
@@ -9,3 +10,27 @@ let rec fibonacci a b evenTotal =
     | sum -> fibonacci sum higher evenTotal
 
 printfn "%i" (fibonacci 1 2 2)
+
+// optimised solution coming from following observation:
+// 2 (even starting point)
+// 8 = 2 * 4
+// 34 = 8 * 4 + 2
+// 144 = 34 * 4 + 8
+
+let rec fib2 curEven evenTotal prevEven =
+    let calc = curEven * 4 + prevEven
+
+    match calc with
+    | calc when calc > 4000_000 -> evenTotal
+    | calc -> fib2 calc (evenTotal + calc) curEven
+
+printfn "%i" (fib2 2 2 0)
+
+// Notes on second solution:
+// Hopefully this will produce same results as first solution towards infinity
+// but with time complexity less than O(n) (likely O(log n)).
+// I can confirm it gives the same result as the brute-force answer above
+// given a limiting number of 4 million, but not above as I am not a mathematician.
+// There is a solution with likely lower complexity than this (using phi)
+// but I would like to stick with this one as I made it myself and also because computers
+// cannot store with absolute accuracy numbers that repeat forever like phi.
